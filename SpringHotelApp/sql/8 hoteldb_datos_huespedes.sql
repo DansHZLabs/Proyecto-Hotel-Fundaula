569a9159e05e2b7fde0e0a10baccd251a7f4b94e
Daniel H. <danielhbas@gmail.com>
2026-05-15 13:19:35 +0200

    Crecion de script sql con datos de prueba para 'huespedes'

---
+++ SpringHotelApp/sql/8 hoteldb_datos_huespedes.sql
@@ -0,0 +1,6 @@
+INSERT INTO huespedes VALUES 
+(NULL, 'Fulanito', 'Perez Martin', 'Gran vía, no.4', '000000000', 'fulanito_Perez@jmail.u'),
+(NULL, 'Menganito', 'Smith Reverte', 'Poeta Querol, n. 8', '111111111','menganito_Smith@jmail.u'),
+(NULL, 'Sotanito', 'Lopez Hernandez', 'Calle Narnia, n. 6', '222222222','sotanito_Hernandez@jmail.u'),
+(NULL, 'Menganita', 'Smith Reverte', 'Calle Cuenca, n. 3', '333333333','menganita_Smith@jmail.u'),
+(NULL, 'Tom', 'Marvolo Riddle', 'Calle Hogwarts, n. 7', '444444444','tom_Riddle@jmail.u');
