4e25a2198a2160c946b80a1226dba812824b9570
Daniel H. <danielhbas@gmail.com>
2026-05-21 10:49:40 +0200

    Creo el script de reservas de prueba

---
+++ SpringHotelApp/sql/11 hoteldb_datos_reservas.sql
@@ -0,0 +1,6 @@
+INSERT INTO reservas (fecha_entrada, fecha_salida, id_huesped, id_habitacion, tipo_pension, estado_reserva, numero_huespedes, observaciones) VALUES 
+('2026-06-01', '2026-06-05', 1, 1, 'ALOJAMIENTO', 'CONFIRMADA', 1, 'Quiere una habitación individual.'),
+('2026-06-10', '2026-06-15', 2, 2, 'MEDIA', 'CONFIRMADA', 2, 'Quiere una habitacion con cama de matrimonio.'),
+('2026-07-01', '2026-07-10', 3, 4, 'COMPLETA', 'PENDIENTE', 2, 'Cliente VIP. Quiere chek-in posterior.'),
+('2026-06-20', '2026-06-25', 4, 7, 'ALOJAMIENTO', 'CONFIRMADA', 1, 'Quiere servicio de desayuno en cama'),
+('2026-08-12', '2026-08-19', 1, 3, 'MEDIA', 'CANCELADA', 2, 'Cancelada por motivos personales.');
